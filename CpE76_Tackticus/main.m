95ccda3fe57a05d2515f9d60d5a941e91539ee3e
Administrator <administrator@ceac316.local>
2013-09-18 14:18:15 +0800

    Initial Commit

---
+++ CpE76_Tackticus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CpE76_Tackticus
+//
+//  Created by Administrator on 9/18/13.
+//  Copyright (c) 2013 Line of Awesome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
